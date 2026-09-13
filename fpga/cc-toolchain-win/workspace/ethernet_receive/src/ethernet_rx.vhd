d8d2a8a2d4aba91424febe30f48620f75f1567cb
drullar <antonio.busuladzich@gmail.com>
2026-09-13 12:30:55 +0300

    Functionaning Ethernet_rx. Has small issue where it skips the source mac in the forwarding logic

--- fpga/cc-toolchain-win/workspace/ethernet_receive/src/ethernet_rx.vhd
+++ fpga/cc-toolchain-win/workspace/ethernet_receive/src/ethernet_rx.vhd
@@ -10,11 +10,12 @@ entity ethernet_rx is
   );
   port (
     clk48              : in std_logic;
-    manchester_data_in : in std_logic; -- non-inverted signal from transmit circuit
+    manchester_data_in : in std_logic;
     data_out           : out std_logic_vector(7 downto 0);
     data_out_valid     : out std_logic
   );
 end ethernet_rx;
+
 architecture Behavioral of ethernet_rx is
 
   subtype rx_frame_header is std_logic_vector(3 downto 0);
@@ -22,6 +23,7 @@ architecture Behavioral of ethernet_rx is
   constant PREAMBLE   : rx_frame_header := "0001";
   constant SFD        : rx_frame_header := "0010";
   constant DST_MAC    : rx_frame_header := "0011";
+  constant TX_DST_MAC : rx_frame_header := "0111";
   constant SRC_MAC    : rx_frame_header := "0100";
   constant ETHER_TYPE : rx_frame_header := "0101";
   constant DATA       : rx_frame_header := "0110";
@@ -30,32 +32,34 @@ architecture Behavioral of ethernet_rx is
   signal data_reg                   : std_logic_vector(7 downto 0) := (others => '0');
   signal bits_read                  : std_logic_vector(3 downto 0) := (others => '0');
   signal data_done                  : std_logic_vector(7 downto 0) := (others => '0');
-  signal cycles_since_last_read_bit : std_logic_vector(2 downto 0) := (others => '0'); -- used to ignore mid bit transitions
+  signal cycles_since_last_read_bit : std_logic_vector(2 downto 0) := (others => '0');
   signal bytes_read                 : std_logic_vector(9 downto 0) := (others => '0');
 
-  signal current_header   : rx_frame_header               := INVALID;
-  signal invert_read_bits : boolean                       := false; -- Identify whether the read bits are received in inverted manner
-  signal frame_dst_mac    : std_logic_vector(47 downto 0) := (others => '0');
+  signal current_header : rx_frame_header := INVALID;
   -- Debug signals
   signal out_bit                   : std_logic;
   signal out_EdgeDetected          : std_logic;
   signal bit_read_to_reg           : std_logic := '0';
   signal header_switch_state_debug : std_logic := '0';
   signal preamble_byte_debug       : std_logic := '0';
+
 begin
   process (clk48)
-    variable cycles_since_last_edge : integer                      := 0;
-    variable v_edge_detected        : boolean                      := false;
-    variable temp_byte              : std_logic_vector(7 downto 0) := (others => '0');
-    variable dst_mac_bytes          : integer                      := 0;
-    variable preamble_bytes         : integer                      := 0;
+    variable cycles_since_last_edge : integer                       := 0;
+    variable v_edge_detected        : boolean                       := false;
+    variable temp_byte              : std_logic_vector(7 downto 0)  := (others => '0');
+    variable dst_mac_bytes          : integer                       := 0;
+    variable preamble_bytes         : integer                       := 0;
+    variable invert_read_bits       : boolean                       := false;
+    variable tx_dst_mac_cnt         : integer                       := 0;
+    variable frame_dst_mac          : std_logic_vector(47 downto 0) := (others => '0');
   begin
     if rising_edge(clk48) then
       header_switch_state_debug <= '0';
       preamble_byte_debug       <= '0';
       v_edge_detected := false;
       in_data        <= in_data(1 downto 0) & manchester_data_in;
-      out_bit        <= 'Z';
+      out_bit        <= '0';
       data_out_valid <= '0';
       temp_byte := (others => '0');
 
@@ -72,9 +76,7 @@ begin
         out_EdgeDetected <= '1';
         cycles_since_last_edge := 0;
 
-        -- Initial read of start of frame transmission or allign based on the clock cycles since last bit - used to read the data in the correct state after mancheter edge transition
         if ((bytes_read = "0000000000") or unsigned(cycles_since_last_read_bit) >= 3) then
-          -- Bits are added to the registed in this IF 
           if (bits_read = "1000") then
             case ETHER_CONTROLLER_MODE is
               when DEBUG =>
@@ -88,15 +90,15 @@ begin
                   when INVALID =>
                     preamble_bytes := 0;
                     if (data_reg = x"AA") then
-                      invert_read_bits <= true;
-                      current_header   <= PREAMBLE;
+                      invert_read_bits := true;
+                      current_header <= PREAMBLE;
                       preamble_bytes := preamble_bytes + 1;
                       header_switch_state_debug <= '1';
                       preamble_byte_debug       <= '1';
                     end if;
                     if (data_reg = x"55") then
-                      invert_read_bits <= false;
-                      current_header   <= PREAMBLE;
+                      invert_read_bits := false;
+                      current_header <= PREAMBLE;
                       preamble_bytes := preamble_bytes + 1;
                       header_switch_state_debug <= '1';
                       preamble_byte_debug       <= '1';
@@ -139,35 +141,59 @@ begin
                       temp_byte := data_reg;
                     end if;
                     case dst_mac_bytes is
-                      when 0      => frame_dst_mac(47 downto 40) <= temp_byte;
-                      when 1      => frame_dst_mac(39 downto 32) <= temp_byte;
-                      when 2      => frame_dst_mac(31 downto 24) <= temp_byte;
-                      when 3      => frame_dst_mac(23 downto 16) <= temp_byte;
-                      when 4      => frame_dst_mac(15 downto 8)  <= temp_byte;
-                      when 5      => frame_dst_mac(7 downto 0)   <= temp_byte;
+                      when 0      => frame_dst_mac(47 downto 40) := temp_byte;
+                      when 1      => frame_dst_mac(39 downto 32) := temp_byte;
+                      when 2      => frame_dst_mac(31 downto 24) := temp_byte;
+                      when 3      => frame_dst_mac(23 downto 16) := temp_byte;
+                      when 4      => frame_dst_mac(15 downto 8)  := temp_byte;
+                      when 5      => frame_dst_mac(7 downto 0)   := temp_byte;
                       when others => null;
                     end case;
                     dst_mac_bytes := dst_mac_bytes + 1;
                     if (dst_mac_bytes = 6) then
-                      if (frame_dst_mac = FPGA_MAC_ADDRESS or frame_dst_mac(40) = '1') then -- Check whether the FPGA is destination or the destination is Multicast/Broadcast
+                      if (frame_dst_mac = FPGA_MAC_ADDRESS or frame_dst_mac(40) = '1') then
                         header_switch_state_debug <= '1';
-                        current_header            <= SRC_MAC;
-                        data_done                 <= data_reg; -- Write full read byte
-                        data_out_valid            <= '1';
+                        current_header            <= TX_DST_MAC;
                       else
                         current_header <= INVALID;
                       end if;
+                      dst_mac_bytes := 0;
                     end if;
+                  when TX_DST_MAC =>
+                    case tx_dst_mac_cnt is
+                      when 0 => temp_byte := frame_dst_mac(47 downto 40);
+                      when 1 => temp_byte := frame_dst_mac(39 downto 32);
+                      when 2 => temp_byte := frame_dst_mac(31 downto 24);
+                      when 3 => temp_byte := frame_dst_mac(23 downto 16);
+                      when 4 => temp_byte := frame_dst_mac(15 downto 8);
+                      when 5 =>
+                        temp_byte := frame_dst_mac(7 downto 0);
+                        current_header <= SRC_MAC;
+                      when others => null;
+                    end case;
+
+                    case tx_dst_mac_cnt is
+                      when 0 to 4 => tx_dst_mac_cnt := tx_dst_mac_cnt + 1;
+                      when others => tx_dst_mac_cnt := 0;
+                    end case;
+
+                    data_done      <= temp_byte;
+                    data_out_valid <= '1';
                   when others =>
-                    data_done      <= data_reg; -- Write full read byte
+                    if (invert_read_bits) then
+                      temp_byte := not data_reg;
+                    else
+                      temp_byte := data_reg;
+                    end if;
+                    data_done      <= temp_byte;
                     data_out_valid <= '1';
                 end case;
                 bytes_read <= std_logic_vector(unsigned(bytes_read) + 1);
                 data_reg   <= in_data(1) & (6 downto 0 => '0');
-                bits_read  <= "0001"; -- Set to 1
+                bits_read  <= "0001";
             end case;
           else
-            data_reg  <= in_data(1) & data_reg(7 downto 1); -- Bitshift right and at new bit as MSBit
+            data_reg  <= in_data(1) & data_reg(7 downto 1);
             bits_read <= std_logic_vector(unsigned(bits_read) + 1);
           end if;
           bit_read_to_reg            <= '1';
@@ -184,15 +210,16 @@ begin
       end if;
 
       if (cycles_since_last_edge > 6) then
-        -- Reset stuff on long idle interval
         data_reg                   <= (others => '0');
         bits_read                  <= (others => '0');
         cycles_since_last_read_bit <= (others => '0');
         data_done                  <= (others => '0');
         bytes_read                 <= (others => '0');
         current_header             <= INVALID;
-        frame_dst_mac              <= (others => '0');
+        frame_dst_mac  := (others => '0');
         preamble_bytes := 0;
+        dst_mac_bytes  := 0;
+        tx_dst_mac_cnt := 0;
         temp_byte      := (others => '0');
       end if;
 
